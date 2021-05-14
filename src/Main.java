86f59f0bac99f3af8dda51899d0e13b12e1fcb8c
Alina <77508351+Alina1103master@users.noreply.github.com>
2021-05-14 08:09:58 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int miles, price; // объявили переменные;
+        price = 2400; // цена билета;
+        miles = price / 20; // формула расчета;
+        System.out.println("miles" + miles);
+    }
+}
